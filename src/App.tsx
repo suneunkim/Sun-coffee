cf7a208c118795faa9f2be21de496276f23c5641
suneunkim <tjsdms89@gmail.com>
2024-04-19 12:42:08 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 import './index.css'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <div>
-        <Register />
-        <Login />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth/register" element={<Register />} />
+      <Route path="/auth/login" element={<Login />} />
+    </Routes>
   )
 }
 
