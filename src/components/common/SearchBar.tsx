a4cdad5ad69684fa9bec9160e19d56cc9e32ba14
suneunkim <tjsdms89@gmail.com>
2024-04-20 16:50:52 +0900

    feat: 홈 전체 레이아웃 구조 설정

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,10 @@
+const SearchBar = () => {
+  return (
+    <section className="bg-[#FFFFFF] w-full">
+      <div className="my-10">SearchBar</div>
+      <div className="my-10">Category</div>
+    </section>
+  )
+}
+
+export default SearchBar
