fa5b625e9d30857f8d751649a5cb6320a253b04b
suneunkim <tjsdms89@gmail.com>
2024-04-21 12:45:58 +0900

    feat: 검색창 레이아웃

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,9 +1,24 @@
+import { CiSearch } from 'react-icons/ci'
+import { PiCoffeeLight } from 'react-icons/pi'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
 const SearchBar = () => {
   return (
-    <section className="bg-[#FFFFFF] w-full">
-      <div className="my-10">SearchBar</div>
-      <div className="my-10">Category</div>
-    </section>
+    <form className="py-5">
+      <div className="flex items-center relative w-[55%]">
+        <CiSearch className="absolute left-2" />
+        <Input
+          type="text"
+          placeholder="Search"
+          className="rounded-xl pl-8 w-[98%] focus:border-gray-600"
+        />
+        <Button className="bg-[#FFA16C] absolute right-0 rounded-full">
+          <PiCoffeeLight className="mr-2 w-5 h-5" />
+          Filter
+        </Button>
+      </div>
+    </form>
   )
 }
 
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus:border-[#FFA16C] focus:outline-none focus:ring-1 focus:ring-[#FFA16C]',
+          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 focus:border-gray-50 focus:outline-none focus:ring-1 focus:ring-gray-50',
           className
         )}
         ref={ref}
