e91b09bf980d83a7cc66d2f5148370ee0fca0c53
suneunkim <tjsdms89@gmail.com>
2024-04-22 16:06:12 +0900

    feat: 이미지 fire strage 등록하기

--- src/components/elements/InputFile.tsx
+++ src/components/elements/InputFile.tsx
@@ -1,11 +1,47 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { storage } from '@/firebase'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import useCurrentUser from '@/hooks/useCurrentUser'
+import { useState } from 'react'
 
-function InputFile() {
+interface Props {
+  onChange: (vaule: string) => void
+  value: string
+}
+
+function InputFile({ onChange, value }: Props) {
+  const userProfile = useCurrentUser()
+  const [isUploaded, setisUploaded] = useState(false) // 파일 선택 중복 금지
+
+  const handleFileSelect = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      await ImageUpload(e.target.files[0])
+    }
+  }
+
+  const ImageUpload = async (file: File) => {
+    const imageRef = ref(storage, `${userProfile?.uid}/${file.name}`)
+    await uploadBytes(imageRef, file)
+    const downloadURL = await getDownloadURL(imageRef)
+    onChange(downloadURL)
+    setisUploaded(true)
+  }
+
   return (
     <div className="grid w-full items-center gap-1.5">
       <Label htmlFor="picture">Picture</Label>
-      <Input id="picture" type="file" />
+      <Input
+        disabled={isUploaded}
+        onChange={handleFileSelect}
+        id="picture"
+        type="file"
+      />
+      {value && (
+        <div className="flex justify-center ">
+          <img className="w-[300px] rounded-lg" src={value} />
+        </div>
+      )}
     </div>
   )
 }
