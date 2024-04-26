cadaef346ab9c06ebbab13b1b5f896479978ff06
suneunkim <tjsdms89@gmail.com>
2024-04-27 08:05:28 +0900

    feat: 상세 페이지 모달 레이아웃 구현

--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -1,12 +1,22 @@
 import { useQueryInitialProducts } from '@/api/fetchProducts'
 import ProductCard from './ProductCard'
 import { Link } from 'react-router-dom'
-import { TypeCategory } from '@/types/common'
+import { TypeCategory, TypeProduct } from '@/types/common'
 
 import ProductCardSkelton from './ProductCardSkelton'
+import { useState } from 'react'
+import DetailModal from './DetailModal'
 
 const ProductList = () => {
-  const categories: TypeCategory[] = ['coffee', 'food', 'non-coffee']
+  const [selectedProduct, setSelectedProduct] = useState<TypeProduct | null>(
+    null
+  )
+  const [showDetailModal, setShowDetailModal] = useState(false)
+  const handleProductSelect = (data: TypeProduct) => {
+    setSelectedProduct(data)
+    setShowDetailModal(true)
+  }
+  const categories: TypeCategory[] = ['coffee', 'non-coffee', 'food']
 
   return (
     <div className="flex justify-center">
@@ -17,10 +27,19 @@ const ProductList = () => {
               <h2>{category}</h2>
               <Link to={`/${category}`}>더보기</Link>
             </div>
-            <ProductPreview category={category} />
+            <ProductPreview
+              handleProductSelect={handleProductSelect}
+              category={category}
+            />
           </div>
         ))}
       </div>
+      {showDetailModal && (
+        <DetailModal
+          product={selectedProduct}
+          onClose={() => setShowDetailModal(false)}
+        />
+      )}
     </div>
   )
 }
@@ -29,15 +48,23 @@ export default ProductList
 
 type ProductPreviwProps = {
   category: TypeCategory
+  handleProductSelect: (data: TypeProduct) => void
 }
 
-const ProductPreview: React.FC<ProductPreviwProps> = ({ category }) => {
+const ProductPreview: React.FC<ProductPreviwProps> = ({
+  category,
+  handleProductSelect,
+}) => {
   const { data, isLoading } = useQueryInitialProducts(category)
   return (
     <div className="grid grid-cols-2 gap-1">
       {isLoading && <ProductCardSkelton productsPerRow={4} />}
       {data?.products.map((product) => (
-        <ProductCard data={product} key={product.id} />
+        <ProductCard
+          data={product}
+          key={product.id}
+          onModal={(data) => handleProductSelect(data)}
+        />
       ))}
     </div>
   )
--- src/components/common/PageLayout.tsx
+++ src/components/common/PageLayout.tsx
@@ -11,7 +11,7 @@ const PageLayout = ({ children }: ChildrenProps) => {
   return (
     <main className="bg-gray-50 flex">
       <Nav customerMenu={customerMenu} />
-      <section className="flex flex-col px-5">
+      <section className="flex flex-col px-5 relative">
         <SearchBar />
         <Category />
         {children}
